41d7bdec9750d22f41f6fe696d633b041857b58e
Yaroslav Usenko <yar.usenko.inc@gmail.com>
2024-11-28 21:04:38 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h1 className="text-3xl font-bold">Hello, Next.js!</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{jsx,mdx}",
+    "./components/**/*.{jsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
