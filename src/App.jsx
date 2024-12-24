12b3726ae8c67f5aa80e27fb264f7359cb5e257c
Yaroslav Usenko <yar.usenko.inc@gmail.com>
2024-12-25 01:39:50 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100 text-center">
+      <div>
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          Інтернет магазин "Ярус"
+        </h1>
+        <p className="text-lg text-gray-600">
+          Скоро відкриття! Завітайте пізніше.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
